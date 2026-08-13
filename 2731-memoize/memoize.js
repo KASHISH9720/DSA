133525c5d713e8603183183d98b070589b5c5608
Kashish <kasishrajpoot841@gmail.com>
2026-08-13 22:51:35 +0530

    Time: 251 ms (72.35%) | Memory: 96 MB (30.45%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = new Map();
+    let callCount = 0;
+
+    const memoized = function(...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        callCount++;
+
+        const result = fn(...args);
+        cache.set(key, result);
+
+        return result;
+    };
+
+    memoized.getCallCount = function() {
+        return callCount;
+    };
+
+    return memoized;
+}
